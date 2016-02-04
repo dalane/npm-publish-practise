040b70ec89366cb88aa6bc0af865ac43613e14ff
Dallas Ewing <dallas.ewing@dalane.co.uk>
2016-02-04 23:21:30 +0000

    changed test back to hello world

--- tests/server-test.js
+++ tests/server-test.js
@@ -7,6 +7,6 @@ describe('GET /', function () {
     beforeEach(function () {});
     afterEach(function () {});
     it("should return \"hello world\"", function (done) {
-        supertest(server).get('/').expect('hello jenkins', done);
+        supertest(server).get('/').expect('hello world', done);
     });
 });
